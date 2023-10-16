045c65ccb033b2cc9d609c4f8005a2ba02e5897f
ambika-garg <70703123+ambika-garg@users.noreply.github.com>
2023-10-16 12:20:29 -0400

    Test

--- dags/refresh-powerbi-datatset.py
+++ dags/refresh-powerbi-datatset.py
@@ -8,20 +8,11 @@ with DAG(
     schedule=None,
     start_date=datetime.datetime(2021, 1, 1),
     catchup=False,
-    # dagrun_timeout=datetime.timedelta(minutes=60),
     tags=["pipeline"],
 ) as dag:
     trigger_refresh = PowerBIDatasetRefreshOperator(
         dataset_id = "af05a415-42ca-49b5-aee4-ac89dcb71a40",
         task_id = "dataset_refresh_task"
     )
-    
-    # (
-    #     azure_synapse_conn_id="azure_synapse_connection",
-    #     task_id="trigger_refresh",
-    #     pipeline_name="Pipeline 1",
-    #     azure_synapse_workspace_dev_endpoint="https://ambika-synapse-workspace.dev.azuresynapse.net",
-    #     deferrable=False 
-    # )
 
     trigger_refresh
