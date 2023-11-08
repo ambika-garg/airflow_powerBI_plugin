f3471a3727842bee95345e27ec4c02b5a3551d42
ambika-garg <70703123+ambika-garg@users.noreply.github.com>
2023-11-08 12:36:11 -0500

    Add KubernetesPod operator dag to test pulling image from Kubernetes secret from other private auth

---
+++ dags/kub_code.py
@@ -0,0 +1,31 @@
+from kubernetes.client import models as k8s
+from airflow import DAG
+from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import KubernetesPodOperator
+from airflow.utils.dates import days_ago
+from airflow.hooks.base_hook import BaseHook
+from airflow.contrib.operators.slack_webhook_operator import SlackWebhookOperator
+
+default_args = {
+    'owner': 'airflow'
+}
+
+ 
+with DAG(
+    dag_id='DockerPrivateImageKubePod',
+    default_args=default_args,
+    schedule_interval="*/5 * * * *",
+    start_date=days_ago(2),
+    catchup=False,
+    tags=['k8s-pod-operator','docker private image repo'],
+) as dag:
+
+    k = KubernetesPodOperator(
+        namespace='default',
+        image="ambikagarg1101/ambikadockerrepo:latest",
+        image_pull_secrets=[k8s.V1LocalObjectReference("testquay")],        
+        name="k8s-pod",
+        task_id="task",
+        is_delete_operator_pod=True,
+        hostnetwork=False,
+        startup_timeout_seconds=1000
+    )
